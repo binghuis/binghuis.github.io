0c9055ed565ab463c81b55949e7f875958f15181
songbinghui <soongbinghui@gmail.com>
2023-05-15 02:04:15 +0800

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { remarkCodeHike } = require("@code-hike/mdx")
+const theme = require("shiki/themes/material-palenight.json")
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [[remarkCodeHike, { theme }]],
+    },
+})
+
+module.exports = withMDX({
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    eslint: { ignoreDuringBuilds: true },
+})
